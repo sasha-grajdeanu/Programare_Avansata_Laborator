e930697902a86de1f27fbc85af345681c336185c
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-03-06 23:30:03 +0200

    Add files via upload

---
+++ Country.java
@@ -0,0 +1,68 @@
+package Homework;
+
+import java.util.Objects;
+
+/**
+ * This class models a country road
+ */
+public class Country extends Road {
+
+    private String condition;
+
+    /**
+     * all-args constructor
+     *
+     * @param start     start location
+     * @param finish    final location
+     * @param length    the length
+     * @param speed     the speed
+     * @param condition the condition of the country road
+     */
+    public Country(Location start, Location finish, int length, int speed, String condition) {
+        super(start, finish, length, speed);
+        this.condition = condition;
+    }
+
+    /**
+     * getter of the condition of the country road
+     *
+     * @return the condition of the country road
+     */
+    public String getCondition() {
+        return condition;
+    }
+
+    /**
+     * setter of the condition of the country road
+     *
+     * @param condition the condition of the country road
+     */
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    /**
+     * overriding method equals()
+     *
+     * @param o an object with which we will compare our class
+     * @return true/false
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Country country = (Country) o;
+        return Objects.equals(condition, country.condition);
+    }
+
+    /**
+     * overriding method toString()
+     *
+     * @return a string who represent a described for the country road
+     */
+    @Override
+    public String toString() {
+        return new StringBuilder().append("Country = {").append("start=").append(super.getStart()).append(", finish=").append(super.getFinish()).append(", speed=").append(super.getSpeed()).append(", length=").append(super.getLength()).append(", condition= ").append(this.condition).append('}').toString();
+    }
+}
---
+++ Location.java
@@ -0,0 +1,110 @@
+package Homework;
+
+
+/**
+ * @author Grajdeanu Alexandru-Cristian
+ * This class models a location with a name and some coordinates.
+ */
+public class Location {
+    private String name;
+    private int coordX;
+    private int coordY;
+
+
+    /**
+     * default constructor
+     */
+    public Location() {
+    }
+
+    /**
+     * all-args constructor
+     *
+     * @param name   name of location
+     * @param coordX coordinate x
+     * @param coordY coordinate y
+     */
+    public Location(String name, int coordX, int coordY) {
+        this.name = name;
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+    /**
+     * setter for coordY
+     *
+     * @param coordY coordinate y
+     */
+    public void setCoordY(int coordY) {
+        this.coordY = coordY;
+    }
+
+    /**
+     * setter for coordX
+     *
+     * @param coordX coordinate x
+     */
+    public void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
+
+    /**
+     * setter for name
+     *
+     * @param name name of location
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * getter for the name of location
+     *
+     * @return name of the location
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * getter for the coordinate X of location
+     *
+     * @return coordinate x of the location
+     */
+    public int getCoordY() {
+        return coordY;
+    }
+
+    /**
+     * getter for the coordinate Y of location
+     *
+     * @return coordinate y of the location
+     */
+    public int getCoordX() {
+        return coordX;
+    }
+
+    /**
+     * overriding method toString()
+     *
+     * @return a string who represent a described for the location
+     */
+    @Override
+    public String toString() {
+        return new StringBuilder().append("Location = {name = ").append(name).append(", coordX = ").append(coordX).append(", coordY = ").append(coordY).append("}").toString();
+    }
+
+    /**
+     * overriding method equals()
+     *
+     * @param o an object with which we will compare our class
+     * @return true/false
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return coordX == location.coordX && coordY == location.coordY && name == location.name;
+    }
+}
