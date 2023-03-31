5c28cd5ef33504458ee69903019c5a6cfe651197
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-03-31 09:26:02 +0300

    repus lombok

--- Lab_5/src/main/java/Homework/Catalog.java
+++ Lab_5/src/main/java/Homework/Catalog.java
@@ -1,6 +1,7 @@
 package Homework;
 
 import Homework.Objects.Document;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -9,17 +10,14 @@ import java.util.List;
 /**
  * This class is a representation of a catalog
  */
+@Data
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Catalog implements Serializable {
     private String name;
     private List<Document> contains = new ArrayList<>();
-
-    public Catalog(String name, List<Document> contains) {
-        this.name = name;
-        this.contains = contains;
-    }
-
-    public Catalog() {
-    }
 
     /**
      * this method put a document in the catalog
@@ -43,53 +41,5 @@ public class Catalog implements Serializable {
             }
         }
         return null;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public List<Document> getContains() {
-        return this.contains;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setContains(List<Document> contains) {
-        this.contains = contains;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Catalog)) return false;
-        final Catalog other = (Catalog) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$name = this.getName();
-        final Object other$name = other.getName();
-        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
-        final Object this$contains = this.getContains();
-        final Object other$contains = other.getContains();
-        if (this$contains == null ? other$contains != null : !this$contains.equals(other$contains)) return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof Catalog;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $name = this.getName();
-        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
-        final Object $contains = this.getContains();
-        result = result * PRIME + ($contains == null ? 43 : $contains.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "Catalog(name=" + this.getName() + ", contains=" + this.getContains() + ")";
     }
 }
--- Lab_5/src/main/java/Homework/Objects/Document.java
+++ Lab_5/src/main/java/Homework/Objects/Document.java
@@ -1,83 +1,20 @@
 package Homework.Objects;
+import lombok.*;
 
 import java.io.Serializable;
+import java.util.Map;
 
 
 /**
  * this class is a representation of a document
  */
+@Data
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode
+@NoArgsConstructor
 public class Document implements Serializable {
     private String name;
     private String path;
     private String id;
-
-    public Document(String name, String path, String id) {
-        this.name = name;
-        this.path = path;
-        this.id = id;
-    }
-
-    public Document() {
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getPath() {
-        return this.path;
-    }
-
-    public String getId() {
-        return this.id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Document)) return false;
-        final Document other = (Document) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$name = this.getName();
-        final Object other$name = other.getName();
-        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
-        final Object this$path = this.getPath();
-        final Object other$path = other.getPath();
-        if (this$path == null ? other$path != null : !this$path.equals(other$path)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof Document;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $name = this.getName();
-        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
-        final Object $path = this.getPath();
-        result = result * PRIME + ($path == null ? 43 : $path.hashCode());
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "Document(name=" + this.getName() + ", path=" + this.getPath() + ", id=" + this.getId() + ")";
-    }
 }
